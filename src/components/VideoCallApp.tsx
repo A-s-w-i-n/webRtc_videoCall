f255ef0f2898c27a37befebd7b0b659e0197ff77
aswin <aswin@kssmart.co>
2025-11-21 12:27:05 +0530

    chanaging url

--- src/components/VideoCallApp.tsx
+++ src/components/VideoCallApp.tsx
@@ -357,10 +357,10 @@ const VideoCallApp = () => {
 
   const connectWebSocket = useCallback(() => {
     try {
-      const websocket : any = new WebSocket("ws://localhost:3001");
-      // const websocket: any = new WebSocket(
-      //   "wss://videocallbackend-sv45.onrender.com"
-      // );
+      // const websocket : any = new WebSocket("ws://localhost:3001");
+      const websocket: any = new WebSocket(
+        "wss://videocallbackend-sv45.onrender.com"
+      );
       wsRef.current = websocket;
 
       websocket.onopen = () => {
